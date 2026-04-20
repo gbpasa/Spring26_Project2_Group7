1a2e8152298057b9dd26d3224314453e77d52913
ajb100 <ajbautista100@csu.fullerton.edu>
2026-04-20 02:00:02 -0700

    updated library packages

--- scripts/01_data_import.R
+++ scripts/01_data_import.R
@@ -1,6 +1,12 @@
 install.packages("tidyverse")
 library(tidyverse)
 
+install.packages("summarytools")
+library(summarytools)
+
+install.packages("ggplot2")
+library(ggplot2)
+
 nhis <- read_csv("data/NHIS _Data_2021.csv")
 
 str(nhis)
