5f24d55b96dfadb9f8499b079a1cbe190ceed604
gbpasa <gpasamonte@csu.fullerton.edu>
2026-04-20 07:51:11 -0700

    Update

--- scripts/01_data_import.R
+++ scripts/01_data_import.R
@@ -1,8 +1,15 @@
 install.packages("tidyverse")
+
+# Load necessary Libraries
 library(tidyverse)
 
+# Import the NHIS 2021 Dataset
 nhis <- read_csv("data/NHIS _Data_2021.csv")
 
+#Initial exploration of data
 str(nhis)
 summary(nhis)
 head(nhis)
+
+
+
